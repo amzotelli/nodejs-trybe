3a3ccde4090adae15fe58ba4e3a2890d325850c8
Amanda Zotelli <amzotelli@gmail.com>
2022-01-17 17:44:14 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket: :sparkles:

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const primeiroCommit = () => console.log('oi');
+primeiroCommit();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
