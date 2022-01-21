ec49d69bb3b3f1770be1edb933b544495b6ed362
Amanda Zotelli <amzotelli@gmail.com>
2022-01-21 17:36:02 -0300

    testando a 3 novamente

--- login.js
+++ login.js
@@ -1,28 +1,38 @@
+const testEmail = (email) => String(email).toLowerCase().match(/\S+@\S+\.\S+/);
+
 const verifyEmail = (req, res, next) => {
   const { email } = req.body;
-  const regexEmail = /\S+@\S+\.\S+/; 
+
   if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
-  if (!regexEmail.test(email)) {
+  
+  if (!testEmail(email)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  next();
+};
+
+const verifyPassword = (req, res, next) => {
+  const { password } = req.body;
+  
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   next();
 };
 
-const verifyPassword = (req, res) => {
-  const { password } = req.body;
-  if (!password) {
-    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  }
-  if (password.length < 6) {
-    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  }
-  const tokenLogin = '7mqaVRXJSp886CGr';
-  res.status(200).json({ token: tokenLogin });
+const tokenLogin = (_req, res) => {
+  res.status(200).json({ token: '7mqaVRXJSp886CGr' });
 };
 
 module.exports = {
   verifyEmail,
   verifyPassword,
+  tokenLogin,
 };
