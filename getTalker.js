02f9e4ba6abb4acd61297422fb845e664f9c2a90
Amanda Zotelli <amzotelli@gmail.com>
2022-01-21 18:18:05 -0300

    acho que agora vai, obrigada Jonathan!!

--- getTalker.js
+++ getTalker.js
@@ -3,6 +3,6 @@ const fs = require('fs').promises;
 module.exports = async (_req, res) => {
   const talker = await fs.readFile('./talker.json');
   const talkerJson = JSON.parse(talker);
-  if (talkerJson.length === 0) res.status(200).send([]);
+  if (talkerJson.length === 0) return res.status(200).send([]);
   return res.status(200).send(talkerJson);
 };
--- login.js
+++ login.js
@@ -1,28 +1,24 @@
-const testEmail = (email) => String(email).toLowerCase().match(/\S+@\S+\.\S+/);
-
 const verifyEmail = (req, res, next) => {
   const { email } = req.body;
-
+  const regexEmail = /\S+@\S+\.\S+/; 
   if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
-  
-  if (!testEmail(email)) {
+  if (!regexEmail.test(email)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
-
   next();
 };
 
 const verifyPassword = (req, res, next) => {
   const { password } = req.body;
-  
   if (!password) {
-    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    return res.status(400).send({ message: 'O campo "password" é obrigatório' });
   }
-
   if (password.length < 6) {
-    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    return res.status(400).send({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
   }
   next();
 };
